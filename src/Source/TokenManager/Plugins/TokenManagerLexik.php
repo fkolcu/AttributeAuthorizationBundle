ef7f54e2321c6d8daa65e0f85b95604de82fa636
fkolcu <furkankolcu@hotmail.com.tr>
2022-09-02 20:24:05 +0300

    install lexik jwt bundle - implement lexik token manager

--- src/Source/TokenManager/Plugins/TokenManagerLexik.php
+++ src/Source/TokenManager/Plugins/TokenManagerLexik.php
@@ -3,14 +3,22 @@
 namespace FK\Bundle\AttributeAuthorizationBundle\Source\TokenManager\Plugins;
 
 use FK\Bundle\AttributeAuthorizationBundle\Source\TokenManager\TokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 
 class TokenManagerLexik implements TokenManagerInterface
 {
+    private JWTEncoderInterface $JWTEncoder;
+
+    public function __construct(JWTEncoderInterface $JWTEncoder)
+    {
+        $this->JWTEncoder = $JWTEncoder;
+    }
+
     /**
      * @inheritDoc
      */
     public function decode(string $token): array
     {
-        // TODO: Implement decode() method.
+        return $this->JWTEncoder->decode($token);
     }
 }
